d5a810d8312c6f6f7eb28adbbbc868ac56429ed1
xiaodong <yunfei_vip@qq.com>
2019-12-06 22:27:48 +0800

    新增this.ctx.grpcStatus状态码枚举

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const grpc = require('grpc');
+
 module.exports = {
   /**
    * origin grpc proto, contains Service and Message
    */
   get grpcProto() {
     return this.app.grpcProto;
+  },
+  /**
+   * Enum of status codes that gRPC can return
+   */
+  get grpcStatus() {
+    return grpc.status;
   },
 };
